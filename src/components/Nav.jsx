57d96edc0d5a22c4718aedc9baf27274a8543013
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-05 18:59:58 -0300

    Add current nav tab identifier

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "/logo.png";
 import "./Nav.scss";
 
+const ROUTES = [
+  { name: "Home", path: "/" },
+  { name: "Projects", path: "/projects" },
+  { name: "Contact", path: "/contact" },
+];
+
 const Nav = () => {
+  const location = useLocation();
+
+  const renderNavLink = ({ name, path }) => {
+    const isCurrent = path === location.pathname;
+    const navLiClassName = isCurrent ? "nav__current" : "";
+    return (
+      <li key={name} className={navLiClassName}>
+        <Link to={path}>{name}</Link>
+      </li>
+    );
+  };
+
   return (
     <nav className="nav">
       <ul>
@@ -11,15 +29,7 @@ const Nav = () => {
             <img alt="Home" src={logo} />
           </Link>
         </li>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+        {ROUTES.map(renderNavLink)}
       </ul>
     </nav>
   );
