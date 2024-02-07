845a50ab2d6f8bb87860eef1998ac1fc46e4d971
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-07 16:49:27 -0300

    Close menu when click outside

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,33 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { ROUTES } from "../utils/constants";
 import logo from "/logo.png";
 import "./Nav.scss";
-import { useState } from "react";
 
 const Nav = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const isCollapsibleNav = window.innerWidth < 1024;
   const [openNav, setOpenNav] = useState(!isCollapsibleNav);
+  const navRef = useRef();
 
-  const handleCloseNav = () => {
+  useEffect(() => {
     isCollapsibleNav && setOpenNav(false);
-  };
+  }, [isCollapsibleNav, location.pathname]);
+
+  useEffect(() => {
+    const handleOutsideClick = ({ target }) => {
+      const current = navRef.current;
+      if (current && !current.contains(target) && isCollapsibleNav && openNav) {
+        setOpenNav(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isCollapsibleNav, openNav]);
 
   const handleLogoClick = () => {
     isCollapsibleNav ? setOpenNav(!openNav) : navigate("/");
@@ -22,15 +37,15 @@ const Nav = () => {
     const isCurrent = path === location.pathname;
     const navLiClassName = isCurrent ? "nav__current" : "";
     return (
-      <li key={name} className={navLiClassName} onClick={handleCloseNav}>
+      <li key={name} className={navLiClassName}>
         <Link to={path}>{name}</Link>
       </li>
     );
   };
 
   return (
-    <nav className="nav">
-      <Link to="/" className="nav__logo" onClick={handleCloseNav}>
+    <nav className="nav" ref={navRef}>
+      <Link to="/" className="nav__logo">
         <img alt="Go to home page" src={logo} />
       </Link>
       {openNav && <ul>{ROUTES.map(renderNavLink)}</ul>}
