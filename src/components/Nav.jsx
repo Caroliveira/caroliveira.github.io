2ec0f5e4dfe1b0f36880fd6c2649549230fbfba7
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-07 16:41:30 -0300

    Make navbar more responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,28 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ROUTES } from "../utils/constants";
 import logo from "/logo.png";
 import "./Nav.scss";
+import { useState } from "react";
 
 const Nav = () => {
+  const navigate = useNavigate();
   const location = useLocation();
+  const isCollapsibleNav = window.innerWidth < 1024;
+  const [openNav, setOpenNav] = useState(!isCollapsibleNav);
+
+  const handleCloseNav = () => {
+    isCollapsibleNav && setOpenNav(false);
+  };
+
+  const handleLogoClick = () => {
+    isCollapsibleNav ? setOpenNav(!openNav) : navigate("/");
+  };
 
   const renderNavLink = ({ name, path }) => {
     const isCurrent = path === location.pathname;
     const navLiClassName = isCurrent ? "nav__current" : "";
     return (
-      <li key={name} className={navLiClassName}>
+      <li key={name} className={navLiClassName} onClick={handleCloseNav}>
         <Link to={path}>{name}</Link>
       </li>
     );
@@ -18,14 +30,13 @@ const Nav = () => {
 
   return (
     <nav className="nav">
-      <ul>
-        <li>
-          <Link to="/">
-            <img alt="Home" src={logo} />
-          </Link>
-        </li>
-        {ROUTES.map(renderNavLink)}
-      </ul>
+      <Link to="/" className="nav__logo" onClick={handleCloseNav}>
+        <img alt="Go to home page" src={logo} />
+      </Link>
+      {openNav && <ul>{ROUTES.map(renderNavLink)}</ul>}
+      <button onClick={handleLogoClick} className="nav__menu">
+        {openNav ? "Close" : "Open"} menu
+      </button>
     </nav>
   );
 };
