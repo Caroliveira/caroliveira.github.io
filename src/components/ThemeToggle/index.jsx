fcdb1bd707cfa7d0a6ce0df152a7e3fe6641881d
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-10 21:06:22 -0300

    Make toggle theme accessible

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../hooks/useThemeContext";
 import styles from "./styles.module.scss";
+import { useTranslation } from "react-i18next";
 
 const ThemeToggle = () => {
+  const { t } = useTranslation();
   const { darkModeOn, setDarkTheme } = useContext(ThemeContext);
 
   return (
@@ -11,6 +13,11 @@ const ThemeToggle = () => {
         type="checkbox"
         checked={darkModeOn}
         onChange={() => setDarkTheme(!darkModeOn)}
+        aria-label={t(
+          darkModeOn
+            ? "themeToggle.enableLightTheme"
+            : "themeToggle.enableDarkTheme"
+        )}
       />
       <span className={styles.toggle__slider} />
     </label>
