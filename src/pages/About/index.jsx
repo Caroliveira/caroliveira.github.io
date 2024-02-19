25bc319b8d9d012165f53c38bf2d8a94750b9f21
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-19 16:47:41 -0300

    Improve about content

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,48 +1,31 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Trans, useTranslation } from "react-i18next";
 import { Button } from "@linaoliveira/design-system";
 import styles from "./styles.module.scss";
-
-const aboutOptions = {
-  feats: (
-    <p>
-      Ser promovida a desenvolvedora sênior em três anos não foi apenas um marco
-      na minha carreira, foi a confirmação do meu comprometimento com a evolução
-      contínua e o desejo de fazer a diferença.
-    </p>
-  ),
-  goals: (
-    <p>
-      Estou em uma jornada para me tornar uma especialista em ReactJs,
-      dedicando-me a criar experiências digitais que se destacam tanto pela
-      usabilidade quanto pelo impacto.
-    </p>
-  ),
-  approach: (
-    <p>
-      Valorizo a colaboração e a troca de ideias entre times de desenvolvedores,
-      produto e designers. Acredito que um projeto só pode atingir seu potencial
-      máximo quando todos os envolvidos têm voz ativa e trabalham em sintonia.
-    </p>
-  ),
-  // skills: null,
-  // testimonials: null,
-};
 
 const About = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { t } = useTranslation("pages");
   const query = new URLSearchParams(location.search);
   const option = query.get("option") || "feats";
+  const aboutOptions = {
+    feats: <p>{t("about.options.feats")}</p>,
+    goals: <p>{t("about.options.goals")}</p>,
+    approach: <p>{t("about.options.approach")}</p>,
+    // skills: null,
+    // testimonials: null,
+  };
+
   return (
     <div className={styles.about}>
-      <h1>Quem é Lina?</h1>
+      <h1>{t("about.who")}</h1>
       <p>
-        Desde que na companhia dos meus colegas de quatro patas, sou alguém que
-        encontra satisfação em transformar café em soluções eficazes de
-        front-end. Atualmente aprofundando meus conhecimentos em testes e
-        performance, busco qualidade em cada projeto, para ter uma ideia do meu
-        trabalho, dê uma olhada na minha{" "}
-        <Link to="/developments">página de desenvolvimentos</Link>.
+        <Trans
+          t={t}
+          i18nKey="about.description"
+          components={{ a: <Link to={"/developments"} /> }}
+        />
       </p>
       <div className={styles.about__options}>
         {Object.keys(aboutOptions).map((key) => (
@@ -52,7 +35,7 @@ const About = () => {
             disabled={key === option}
             onClick={() => navigate({ search: `?option=${key}` })}
           >
-            Proezas
+            {t(`about.${key}`)}
           </Button>
         ))}
       </div>
